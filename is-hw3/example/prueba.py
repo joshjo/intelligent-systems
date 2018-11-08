87f48d08e2c716a919efd381b5301acb4a69d455
Josue Ttito <josue.ttito@ucsp.edu.pe>
2018-11-08 00:22:11 -0500

    add backpropagation algorithm

--- is-hw3/example/prueba.py
+++ is-hw3/example/prueba.py
@@ -39,10 +39,45 @@ def get_minibatch(doc_stream, size):
 df = pd.read_csv('shuffled_movie_data.csv')
 
 
-X = df['review']
-y = df['sentiment']
+def featureVecMethod(words, model, num_features):
+    # Pre-initialising empty numpy array for speed
+    featureVec = np.zeros(num_features,dtype="float32")
+    nwords = 0
+
+    #Converting Index2Word which is a list to a set for better speed in the execution.
+    index2word_set = set(model.wv.index2word)
+
+    for word in  words:
+        if word in index2word_set:
+            nwords = nwords + 1
+            featureVec = np.add(featureVec,model[word])
+
+    # Dividing the result by number of words to get average
+    featureVec = np.divide(featureVec, nwords)
+    return featureVec
+
+
+def getAvgFeatureVecs(reviews, model, num_features):
+    counter = 0
+    reviewFeatureVecs = np.zeros((len(reviews),num_features),dtype="float32")
+    for review in reviews:
+        # Printing a status message every 1000th review
+        if counter%1000 == 0:
+            print("Review %d of %d"%(counter,len(reviews)))
+
+        reviewFeatureVecs[counter] = featureVecMethod(review, model, num_features)
+        counter = counter+1
+
+    return reviewFeatureVecs
+
+
+# X = df['review']
+# y = df['sentiment']
 
 
 if __name__ == '__main__':
-    print(tokenizer('This :) is a <a> test! :-)</br>'))
-    print(X[2])
+    X = list(df['review'])
+    xx = []
+    for row in X:
+        xx.append(tokenizer(i))
+    model = Word2Vec(xx, size=100, window=5, min_count=1, workers=4)
