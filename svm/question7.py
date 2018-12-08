286d9e84787fcea33391c260a67f45efe83239e2
Josue Ttito <josue.ttito@ucsp.edu.pe>
2018-12-07 21:31:04 -0500

    add SVM

--- svm/question7.py
+++ svm/question7.py
@@ -1,17 +1,33 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy import linalg as LA
 
 
 ax = plt.axes()
 sqrt_05 = np.sqrt(0.5)
 
-plt.scatter([1, 4, 4, 2, 3], [6, 9, 6, 2, 1], marker='x')
-plt.scatter([5, 9, 9, 0], [1, 5, 1, 3], marker='o')
+y0 = [[1, 6], [4, 9], [4, 6], [2, 2], [3, 1]]
+y1 = [[5, 1], [9, 5], [9, 1], [0, 3]]
+
+plt.scatter(*zip(*y0), marker='x')
+plt.scatter(*zip(*y1), marker='o')
 
 x = np.linspace(0, 10)
 
 ax.plot(x, x - 1, label=r'$h_1$')
 
-plt.legend()
-plt.show()
-    
+
+def rect(x):
+    return x[0] - x[1] - 1
+
+
+def dist(point):
+    return abs(rect(x)) / LA.norm(point, 2)
+
+
+if __name__ == '__main__':
+    print([rect(i) for i in y0])
+    print([rect(i) for i in y1])
+    print(
+        [rect(i) for i in [[2, 2], [3, 1], [0, 3]]])
+    # plt.show()
